589adfd6415848e48c3e5905fcb0aa40753d6222
DarioBoccardo <dario.boccardo@iisviolamarchesini.edu.it>
2023-10-02 17:19:35 +0200

    Aggiungere i file di progetto.

---
+++ Conversione_Binario_Decimale/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversione_Binario_Decimale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] indirizzoIp = new int[4];
+        }
+        static bool Sequenza()
+        {
+            return true;
+        }
+        /*static bool[] ConvertDeBi()
+        {
+            return;
+        }*/
+        static int ConvertDpToInt() 
+        {
+            return 0;
+        }
+        static int ConvertBiInt() 
+        {
+            return 0; 
+        }
+        /*static int[] ConvertBiDe()
+        {
+            
+        }*/
+    }
+}
