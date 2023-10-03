2faddd9d91723b2286e17c13a43c89056234582c
DarioBoccardo <dario.boccardo@iisviolamarchesini.edu.it>
2023-10-03 15:36:58 +0200

    Commit4

--- Conversione_Binario_Decimale/Program.cs
+++ Conversione_Binario_Decimale/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,28 +11,47 @@ namespace Conversione_Binario_Decimale
     {
         static void Main(string[] args)
         {
-            int[] indirizzoIp = new int[4];
-            bool[] bn = new bool[32];
+            int[] dp = new int[4];
 
             Console.WriteLine("Inserisci l'indirizzo IP in decimale puntato");
-            for(int i = 0; i < indirizzoIp.Length; i++)
+            for(int i = 0; i < dp.Length; i++)
             {
-                indirizzoIp[i] = Convert.ToInt32(Console.ReadLine());
+                dp[i] = Convert.ToInt32(Console.ReadLine());
             }
+            ConvertDpToInt(); // Converte la stringa inserita dall'utente in int
+            bool[] bn = ConvertDpBi(dp); // Converte l'intero in decimale puntato ad booleano in binario
+            ConvertBiInt(bn); // Converte l'array di bool in un array int
+            ConvertBiDp(); // Converte il binario booleano in un decimale
+
+            Console.ReadLine();
         }
-        /*static bool[] ConvertDeBi()
+        static int ConvertDpToInt()
         {
-            return;
-        }*/
-        /*static int ConvertDpToInt() 
-        {
+
             return 0;
         }
-        static int ConvertBiInt() 
+        static bool[] ConvertDpBi(int[] dp)
         {
+            int resto = 0, cont = 31;
+            bool[] bn = new bool[32];
+            for(int i = 0; i < dp.Length; i++)
+            {
+                do
+                {
+                    resto = dp[i] % 2;
+                    dp[i] = dp[i] / 2;
+                    bn[cont] = resto == 0;
+                    cont--;
+                } while (dp[i] < 2);
+            }
+            return bn;
+        }
+        static int ConvertBiInt(bool[] bn) 
+        {
+
             return 0; 
-        }*/
-        /*static int[] ConvertBiDe()
+        }
+        /*static int[] ConvertBiDp()
         {
             
         }*/
